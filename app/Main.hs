46d5a5a43d0e5d247732ac364e9c20d037292726
Karl Wang <kawang@ucsd.edu>
2021-11-08 23:31:37 -0800

    Success render

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,7 @@ import Brick.Widgets.Border (borderWithLabel, vBorder)
 import Brick.Widgets.Border.Style (unicode)
 import Brick.Widgets.Center (center)
 import Request
-
--- import Screen (problemsList)
-
-ui :: Widget ()
-ui =
-  withBorderStyle unicode $
-    borderWithLabel
-      (str "Hello!")
-      (center (str "LeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeft\nLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeftLeft") <+> vBorder <+> center (str "Right"))
+import Screen (tui)
 
 main :: IO ()
--- main = simpleMain problemsList
-main = simpleMain ui
-
--- main = someFunc
-
--- someFunc
+main = tui
--- src/Screen.hs
+++ src/Screen.hs
@@ -3,14 +3,61 @@
 module Screen where
 
 import Brick
-import Brick.Widgets.Center (center)
 import Control.Lens hiding ((.=))
 import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Aeson.Lens
-import Data.Text
+import qualified Data.Text as T
+import Graphics.Vty.Input.Events
 import Request (getResponseBody, getUserInfo, maybeToIO)
-import Prelude hiding (head)
+
+tui :: IO ()
+tui = do
+  initialState <- buildInitialState
+  endState <- defaultMain tuiApp initialState
+  print endState
+
+data TuiState = TuiState {tuiStateUserInfo :: [(String, String)]}
+  deriving (Show, Eq)
+
+type ResourceName = String
+
+tuiApp :: App TuiState e ResourceName
+tuiApp =
+  App
+    { appDraw = drawTui,
+      appChooseCursor = showFirstCursor,
+      appHandleEvent = handleTuiEvent,
+      appStartEvent = return,
+      appAttrMap = const $ attrMap mempty []
+    }
+
+buildInitialState :: IO TuiState
+buildInitialState =
+  do
+    obj <- getResponseBody getUserInfo
+    return $
+      TuiState
+        { tuiStateUserInfo = case obj of
+            Bool False -> [("Error occured", "")]
+            _ ->
+              let username = obj ^? key "user" . key "username"
+               in case username of
+                    Just (String x) -> [("username: ", init . tail . show $ x)]
+                    _ -> [("Error occured", "")]
+        }
+
+drawTui :: TuiState -> [Widget ResourceName]
+drawTui ts = map (str . uncurry (++)) $ tuiStateUserInfo ts
+
+handleTuiEvent :: TuiState -> BrickEvent n e -> EventM n (Next TuiState)
+handleTuiEvent s e =
+  case e of
+    VtyEvent vtye ->
+      case vtye of
+        EvKey (KChar 'q') [] -> halt s
+        _ -> continue s
+    _ -> continue s
 
 -- problemsList :: Widget ()
 -- problemsList = do
